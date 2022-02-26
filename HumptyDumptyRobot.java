1d3de15d322c4bb4810c88add4f6717852d3c39f
airtigers <89110180+airtigers@users.noreply.github.com>
2022-02-26 12:40:13 -0500

    Update HumptyDumptyRobot.java

--- HumptyDumptyRobot.java
+++ HumptyDumptyRobot.java
@@ -9,10 +9,13 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+
 //adding ps4 controls
 import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 
 
 /**
@@ -22,12 +25,12 @@ import edu.wpi.first.wpilibj.Timer;
 public class Robot extends TimedRobot {
   //driving motors
   private final PWMTalonFX m_topLeft = new PWMTalonFX(4); // the top left motor - intiailizes the motor
-  private final PWMTalonFX m_bottomLeft = new PWMTalon(0); // the bottom left motor - intiailizes the motor
+  private final PWMTalonFX m_bottomLeft = new PWMTalonFX(0); // the bottom left motor - intiailizes the motor
   MotorControllerGroup leftMotors = new MotorControllerGroup(m_topLeft, m_bottomLeft); // groups both of the motors on the left side to an object we'll refer later when we use differential drive
 
   private final PWMTalonFX m_topRight = new PWMTalonFX(2); // the top right motor - intiailizes the motor
   private final PWMTalonFX m_bottomRight = new PWMTalonFX(3); // the bottom right motor - intiailizes the motor
-  MotorControllerGroup rightMotors = new MotorControllerGroup(m_topRight, m_bottomRight); // groups both of the motors on the left side to an object we'll refer later 
+  private final MotorControllerGroup rightMotors = new MotorControllerGroup(m_topRight, m_bottomRight); // groups both of the motors on the left side to an object we'll refer later 
 
   private final DifferentialDrive drivingMotors = new DifferentialDrive(leftMotors, rightMotors); // differential drive for the motors (can now use tank drive)
 
@@ -48,7 +51,7 @@ public class Robot extends TimedRobot {
     /* We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.*/
-    m_rightMotor.setInverted(true);
+    rightMotors.setInverted(true);
     //m_rShooterMotor.setInverted(true);
   }
 
@@ -56,29 +59,35 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit()
   {
-    ps4.setRumble(kLeftRumble, 0.5); // rumbles the ps4 controller (we have access to this method despite it being in GenericHID because the PS4Controller class extends the class "GenericHID")
-    ps4.setRumble(kRightRumble, 0.5); // rumbles the ps4 controller
+    // ps4.setRumble(kLeftRumble, 0.5); // rumbles the ps4 controller (we have access to this method despite it being in GenericHID because the PS4Controller class extends the class "GenericHID")
+    // ps4.setRumble(kRightRumble, 0.5); // rumbles the ps4 controller
   }
   
-  // this method is ran periodically while the robot is in teleop
+  // this method is ran periodically if the robot is in teleop
   @Override
   public void teleopPeriodic() {
     /* robot uses tank drive
       that means that the left stick on the PS4 controller controls the motors/wheels on the left side of the robot, and right stick controls the right side
     */
+    ps4.setRumble(RumbleType.kLeftRumble, 0.9);
+    ps4.setRumble(RumbleType.kRightRumble, 0.9);
 
-    drivingMotors.tankDrive(-1* ps4.getLeftY(), -1 * ps4.getRightY()); // we want positve (+) values to move forwards, and negative (-) to move backwards
+    drivingMotors.tankDrive(-0.4* ps4.getLeftY(), -0.4  * ps4.getRightY()); // we want positve (+) values to move forwards, and negative (-) to move backwards
 
-    m_lShooterMotor.set(0.5); // spins the motor
-    m_rShooterMotor.set(0.5); // spins the motor
-
-    if (ps4.getCrossButtonPress() == true) // if you press the X button (cross) on the controller, it'll stop the motor from spinning.
+    while (ps4.getL2Button() == true) 
+    {
+      m_lShooterMotor.set(-1); // spins the motor
+      m_rShooterMotor.set(-1); // spins the motor
+    }
+    
+    while (ps4.getR2ButtonPressed() == true) // if you press the X button (cross) on the controller, it'll stop the motor from spinning.
     {
       m_lShooterMotor.stopMotor();
       m_rShooterMotor.stopMotor();
     }
+    System.out.println("teleop works");
 
-    m_externalMotorDrive.arcadeDrive(-ps4.getRightY(), 0.0); //shooter motor controller
+    //m_externalMotorDrive.arcadeDrive(-ps4.getRightY(), 0.0); //shooter motor controller
 
     //m_externalMotorDrive.setSpeed()
     
@@ -88,25 +97,32 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit()
   {
-    ps4.setRumble(kLeftRumble, 0.9)
-    time.reset()
+    time.reset();
     time.start();
   } 
 
-  // this method is ran periodically while the robot is in autonomous
+  // this method is ran periodically if the robot is in autonomous
   @Override
   public void autonomousPeriodic()
   {
-    if (time.get() <= 5.0) // our autonomous period will run for five (5) seconds 
+    if (time.get() <= 15.0) // our autonomous period will run for five (5) seconds 
     {
-      drivingMotors.tankDrive(0.5, 0.5);
+      System.out.println("autonomous works: 3 sec");
+      System.out.println(time.get());
+      drivingMotors.tankDrive(-0.5, 0.5);
     }
     else
     {
       drivingMotors.stopMotor();
       time.stop();
-      ps4.setRumble(kLeftRumble, 0.5); // rumble the controller
-      ps4.setRumble(kRightRumble, 0.5); // rumble the controller
+      // ps4.setRumble(kLeftRumble, 0.5); // rumble the controller
+      // ps4.setRumble(kRightRumble, 0.5); // rumble the controller
     }
+  }
+
+  @Override
+  public void testPeriodic()
+  {
+    System.out.println("hi");
   }
 }
