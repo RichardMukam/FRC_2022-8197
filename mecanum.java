395051a720d44f193dcf89fd3baf76cd1c35a294
spicylemonade <84095175+spicylemonade@users.noreply.github.com>
2022-03-03 17:37:14 -0500

    finished mecanum drive, needs revision.

--- mecanum.java
+++ mecanum.java
@@ -5,19 +5,25 @@
 package frc.robot;
 
 
-
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
-//adding ps4 controls
+//Import for PS4
 import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.PS4Controller;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+
+//Import for Limelight
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+
+//mecanum drive
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase;
+
 
 
 /**
@@ -25,37 +31,30 @@ import edu.wpi.first.wpilibj.GenericHID.RumbleType;
  * arcade steering.
  */
 public class Robot extends TimedRobot {
+
   //driving motors
   private final PWMTalonFX m_topLeft = new PWMTalonFX(4); // the top left motor - intiailizes the motor
  // private final PWMSparkMax m_topLeft = new PWMSparkMax(4);
   private final PWMTalonFX m_bottomLeft = new PWMTalonFX(0); // the bottom left motor - intiailizes the motor
  // private final PWMSparkMax m_bottomLeft = new PWMSparkMax(4);
-  MotorControllerGroup leftMotors = new MotorControllerGroup(m_topLeft, m_bottomLeft); // groups both of the motors on the left side to an object we'll refer later when we use differential drive
-
   private final PWMTalonFX m_topRight = new PWMTalonFX(2); // the top right motor - intiailizes the motor
   private final PWMTalonFX m_bottomRight = new PWMTalonFX(3); // the bottom right motor - intiailizes the motor
-  private final MotorControllerGroup rightMotors = new MotorControllerGroup(m_topRight, m_bottomRight); // groups both of the motors on the left side to an object we'll refer later 
-
-  private final DifferentialDrive drivingMotors = new DifferentialDrive(leftMotors, rightMotors); // differential drive for the motors (can now use tank drive)
-
-  //shooter motors
-  private final PWMTalonFX m_lShooterMotor = new PWMTalonFX(5); // the climber motor on the left side - intiailizes the motor
-  private final PWMTalonFX m_rShooterMotor = new PWMTalonFX(1); // the climber motor on the right side - intiailizes the motor
-  
-  //private final DifferentialDrive m_externalMotorDrive = new DifferentialDrive(m_lShooterMotor, m_rShooterMotor);
-  
   //adding ps4 controls
   private final PS4Controller ps4 = new PS4Controller(0);
 
   //creating an object for the timer
   private final Timer time = new Timer();
 
+  //mecanum
+  private MecanumDrive Mecanum;
   @Override
   public void robotInit() {
+    Mecanum = new MecanumDrive(m_topLeft, m_bottomLeft, m_topRight, m_bottomRight);
+    //initialize mecanum drives motors
+    NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").setNumber(3);
     /* We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.*/
-    rightMotors.setInverted(true);
     //m_rShooterMotor.setInverted(true);
   }
 
@@ -63,6 +62,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit()
   {
+    
     // ps4.setRumble(kLeftRumble, 0.5); // rumbles the ps4 controller (we have access to this method despite it being in GenericHID because the PS4Controller class extends the class "GenericHID")
     // ps4.setRumble(kRightRumble, 0.5); // rumbles the ps4 controller
   }
@@ -70,30 +70,16 @@ public class Robot extends TimedRobot {
   // this method is ran periodically if the robot is in teleop
   @Override
   public void teleopPeriodic() {
-    /* robot uses tank drive
+    /* robot uses  drive
       that means that the left stick on the PS4 controller controls the motors/wheels on the left side of the robot, and right stick controls the right side
     */
     ps4.setRumble(RumbleType.kLeftRumble, 0.9);
     ps4.setRumble(RumbleType.kRightRumble, 0.9);
 
-    drivingMotors.tankDrive(-0.4* ps4.getLeftY(), -0.4  * ps4.getRightY()); // we want positve (+) values to move forwards, and negative (-) to move backwards
+    Mecanum.driveCartesian(ps4.getLeftY(), ps4.getLeftX(), ps4.getRightX(), 0.0);
+    // mecanum drive. takes input of left joystick to move left right/foreward back. takes left x input to rotate(I assume). last input is gyroAngle(dont know use)
 
-    while (ps4.getL2Button() == true) 
-    {
-      m_lShooterMotor.set(-1); // spins the motor
-      m_rShooterMotor.set(-1); // spins the motor
-    }
     
-    while (ps4.getR2ButtonPressed() == true) // if you press the X button (cross) on the controller, it'll stop the motor from spinning.
-    {
-      m_lShooterMotor.stopMotor();
-      m_rShooterMotor.stopMotor();
-    }
-    System.out.println("teleop works");
-
-    //m_externalMotorDrive.arcadeDrive(-ps4.getRightY(), 0.0); //shooter motor controller
-
-    //m_externalMotorDrive.setSpeed()
     
   }
 
@@ -101,8 +87,7 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit()
   {
-    time.reset();
-    time.start();
+
   } 
 
   // this method is ran periodically if the robot is in autonomous
@@ -113,14 +98,15 @@ public class Robot extends TimedRobot {
     {
       System.out.println("autonomous works: 3 sec");
       System.out.println(time.get());
-      drivingMotors.tankDrive(-0.5, 0.5);
+      Mecanum.driveCartesian(0.5, 0.5, 0.0, 0.0);
+
+
     }
     else
     {
-      drivingMotors.stopMotor();
       time.stop();
-      // ps4.setRumble(kLeftRumble, 0.5); // rumble the controller
-      // ps4.setRumble(kRightRumble, 0.5); // rumble the controller
+      Mecanum.stopMotor();
+      //stop motor
     }
   }
 
