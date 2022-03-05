fad0f68a3ea74a2e77a571de01182da58dc7f12a
RichardMukam <77300333+RichardMukam@users.noreply.github.com>
2022-03-04 22:17:34 -0500

    Update OtherMotor.java

--- OtherMotor.java
+++ OtherMotor.java
@@ -9,14 +9,12 @@ import com.ctre.phoenix.motorcontrol.Faults; //debugger
 import com.ctre.phoenix.motorcontrol.InvertType; //inverted
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX; //motorcontroller
 
-
 //REV sparkmotors
 import edu.wpi.first.wpilibj.MotorSafety;
 import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
-
-
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
@@ -35,161 +33,67 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class Robot extends TimedRobot {
     // right side
-    WPI_VictorSPX _rightBack = new WPI_VictorSPX(1);
-    WPI_VictorSPX _rightFront = new WPI_VictorSPX(2);
-    MotorControllerGroup _driveRight = new MotorControllerGroup(_rightBack, _rightFront);
+    WPI_VictorSPX rightBack = new WPI_VictorSPX(1);
+    WPI_VictorSPX rightFront = new WPI_VictorSPX(2);
+    MotorControllerGroup rightMotors = new MotorControllerGroup(rightBack, rightFront);
     // left side
-    WPI_VictorSPX _leftBack = new WPI_VictorSPX(3);
-    WPI_VictorSPX _leftFront = new WPI_VictorSPX(4);
-    MotorControllerGroup _driveLeft = new MotorControllerGroup(_leftBack, _leftFront);
+    WPI_VictorSPX leftBack = new WPI_VictorSPX(3);
+    WPI_VictorSPX leftFront = new WPI_VictorSPX(4);
+    MotorControllerGroup leftMotors = new MotorControllerGroup(leftBack, leftFront);
     // set up for differential drive
-    DifferentialDrive _diffDrive = new DifferentialDrive(_driveLeft, _driveRight); //this was originally driveRight,driveLeft, but should've been driveLeft,driveRight
+    DifferentialDrive _diffDrive = new DifferentialDrive(leftMotors, rightMotors); //this was originally driveRight,driveLeft, but should've been driveLeft,driveRight
     PWMTalonSRX _feeder = new PWMTalonSRX(7);
-    PWMTalonSRX _shooterhood = new PWMTalonSRX(4);
     PWMSparkMax _shooter = new PWMSparkMax(5);
-
     
 
     //timer (for autonomous)
     Timer time = new Timer();
 
     // controllers
-    Joystick _joystick = new Joystick(0);
     PS4Controller ps4 = new PS4Controller(0);
-
-    // debug
-    Faults _faults_L = new Faults();
-    Faults _faults_R = new Faults();
-    
+    Joystick joystick = new Joystick(1);
     
     @Override
     public void autonomousPeriodic()
     {
-        /*if (time.get() < 5.0)
+        if (time.get() < 5.0)
         {
             
-        }*/
-
-        /*
-        if(_Ultrasonic.getRangeInches() > 12) {
-           // The manuever the robot should do
-           _diffDrive.tankDrive(.5, .5);
-           // *.5 stands for the speed meaning that the right and left speed should be .5
-
         }
-        else {
-           //if not, the robot should move
-           _diffDrive.tankDrive(0, 0);
-        }*/
     }
     @Override
     public void teleopPeriodic() {
+        _diffDrive.tankDrive(ps4.getLeftY(), ps4.getRightY());
 
-        //String work = ""; debug
-
-        /* get gamepad stick values */
-        // multiples the y value (raw axis) of the L-stick of the joystick by -1 to get
-        // a final result
-        // double turn = +1 * _joystick.getRawAxis(0); /* positive is right */
-        // //original value is +1 use this when using arcade drive
-        double lSide = -1 * ps4.getLeftY(); // + is forward, getting the y-value of the L-stick, moving up goes fo
-        double rSide = +1 * ps4.getRightY(); // + is forwad, gets y-value of R-stick
-        boolean feederButton = ps4.getRawButtonPressed(2);
-        boolean shootButton = ps4.getRawButton(1);
-        boolean hoodButton = ps4.getRawButtonPressed(3);
-        //boolean feederButton = ps4.getCrossButton();
-       // boolean shootButton = ps4.getCrossButton();
-       // boolean hoodButton = ps4.getCrossButton();
-
-        /* deadband gamepad 10% */ // basically mistaken movement of L-stick or R-stick
-        if (Math.abs(lSide) < 0.10) {
-            lSide = 0;
-        }
-        if (Math.abs(rSide) < 0.10) {
-            rSide = 0;
-        }
-
-        while(feederButton)
+        if (ps4.getCrossButtonPressed())
         {
-            _feeder.set(-1.0);
+            
         }
-        while(shootButton)
-        {
-            _shooter.set(-1.0);
-        }
-        while(hoodButton)
-        {
-            _shooterhood.set(-1.0);
-        }
-        /*
-
-         * if (Math.abs(turn) < 0.10) { //for arcade
-         * turn = 0;
-         * }
-         */
-
-        /* drive robot */
-        // _diffDrive.arcadeDrive(forw, turn); //arcade drive
-        _diffDrive.tankDrive(lSide, rSide);
-
-        /*
-         * [2] Make sure Gamepad Forward is positive for FORWARD, and GZ is positive for
-         * RIGHT
-         */
-        // work += " GF:" + forw; //+ " GT:" + turn
-
-        /* get sensor values */
-        // double leftPos = _leftFront.GetSelectedSensorPosition(0);
-        // double rghtPos = _rghtFront.GetSelectedSensorPosition(0);
-        //double leftVelUnitsPer100ms = _leftBack.getSelectedSensorVelocity(0);
-        //double rightVelUnitsPer100ms = _rightBack.getSelectedSensorVelocity(0);
-
-        //work += " L:" + leftVelUnitsPer100ms + " R:" + rightVelUnitsPer100ms;
-
-        /*
-         * drive motor at least 25%, Talons will auto-detect if sensor is out of phase
-         */
-        // *faults are for debugging, checking problems
-        _leftBack.getFaults(_faults_L); // *use this implementation
-        _rightBack.getFaults(_faults_R); // *use this implementation
-
-       /* if (_faults_L.SensorOutOfPhase) { // *use this implementation
-            work += " L sensor is out of phase";
-        }
-        if (_faults_R.SensorOutOfPhase) { // *use this implementation
-            work += " R sensor is out of phase";
-        }*/
-
+        //create a method for the motors to spin
+       
     }
 
     @Override
     public void robotInit() {
+        Ultrasonic.setAutomaticMode(true);
         /* factory default values */
-        _rightBack.configFactoryDefault();
-        _rightFront.configFactoryDefault();
-        _leftBack.configFactoryDefault();
-        _leftFront.configFactoryDefault();
+        rightBack.configFactoryDefault();
+        rightFront.configFactoryDefault();
+        leftBack.configFactoryDefault();
+        leftFront.configFactoryDefault();
 
       /* [3] flip values so robot moves forward when stick-forward/LEDs-green */
-        _rightBack.setInverted(false); // !< Update this
-        _leftBack.setInverted(false); // !< Update this
+        rightBack.setInverted(false); // !< Update this
+        leftBack.setInverted(false); // !< Update this
 
         /* set up followers */
-        _rightFront.follow(_rightBack);
-        _leftFront.follow(_leftBack);
-
-
+        rightFront.follow(rightBack);
+        leftFront.follow(leftBack);
 
         /*
          * set the invert of the followers to match their respective master controllers
          */
-        _rightFront.setInverted(InvertType.FollowMaster);
-        _leftFront.setInverted(InvertType.FollowMaster);
-
-        /*
-         * [4] adjust sensor phase so sensor moves positive when Talon LEDs are green
-         */
-        _rightBack.setSensorPhase(false);
-        _leftBack.setSensorPhase(true);
+        rightFront.setInverted(InvertType.FollowMaster);
+        leftFront.setInverted(InvertType.FollowMaster);
     }
 }
