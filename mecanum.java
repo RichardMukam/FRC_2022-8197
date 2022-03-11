c2bf42ca0444d8524b31c3e3b469f75bc72fdc87
airtigers <89110180+airtigers@users.noreply.github.com>
2022-03-10 21:29:50 -0500

    Update mecanum.java

--- mecanum.java
+++ mecanum.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 //Import for PS4
 import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.PS4Controller;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 
@@ -21,9 +22,13 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-//mecanum drive
+//Import for mecanumdrive
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.drive.RobotDriveBase;
+
+//spark motors
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 
@@ -33,15 +38,27 @@ import edu.wpi.first.wpilibj.drive.RobotDriveBase;
  */
 public class Robot extends TimedRobot {
 
-  //driving motors
+  /*//driving motors
   PWMSparkMax m_topLeft = new PWMSparkMax(4); // the top left motor - intiailizes the motor
  // private final PWMSparkMax m_topLeft = new PWMSparkMax(4);
   PWMSparkMax m_bottomLeft = new PWMSparkMax(3); // the bottom left motor - intiailizes the motor
  // private final PWMSparkMax m_bottomLeft = new PWMSparkMax(4);
   PWMSparkMax m_topRight = new PWMSparkMax(2); // the top right motor - intiailizes the motor
   PWMSparkMax m_bottomRight = new PWMSparkMax(1); // the bottom right motor - intiailizes the motor
-  //adding ps4 controls
+  //adding ps4 controls*/
+
+
+  //driving motors
+  CANSparkMax  m_topLeft = new CANSparkMax(1,MotorType.kBrushless); // the top left motor - intiailizes the motor
+ // private final PWMSparkMax m_topLeft = new PWMSparkMax(4);
+ CANSparkMax  m_bottomLeft = new CANSparkMax(2, MotorType.kBrushless); // the bottom left motor - intiailizes the motor
+ // private final PWMSparkMax m_bottomLeft = new PWMSparkMax(4);
+ CANSparkMax  m_topRight = new CANSparkMax(4, MotorType.kBrushless); // the top right motor - intiailizes the motor
+ CANSparkMax  m_bottomRight = new CANSparkMax(3, MotorType.kBrushless); // the bottom right motor - intiailizes the motor
+
+  //adding Joystick
   PS4Controller ps4 = new PS4Controller(0);
+  Joystick joystick = new Joystick(0);
 
   //creating an object for the timer
   Timer time = new Timer();
@@ -51,6 +68,7 @@ public class Robot extends TimedRobot {
   @Override
   public void robotInit() {
     Mecanum = new MecanumDrive(m_topLeft, m_bottomLeft, m_topRight, m_bottomRight);
+
     //initialize mecanum drives motors
     NetworkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>").setNumber(3);
     /* We need to invert one side of the drivetrain so that positive voltages
@@ -63,7 +81,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopInit()
   {
-    
+
     // ps4.setRumble(kLeftRumble, 0.5); // rumbles the ps4 controller (we have access to this method despite it being in GenericHID because the PS4Controller class extends the class "GenericHID")
     // ps4.setRumble(kRightRumble, 0.5); // rumbles the ps4 controller
   }
@@ -79,6 +97,48 @@ public class Robot extends TimedRobot {
 
     Mecanum.driveCartesian(ps4.getLeftY(), ps4.getLeftX(), ps4.getRightX(), 0.0);
     // mecanum drive. takes input of left joystick to move left right/foreward back. takes left x input to rotate(I assume). last input is gyroAngle(dont know use)
+
+    //raw mecanum code(move forward)
+    /*if (ps4.getLeftY() < 0.1){
+      m_topLeft.set(ps4.getLeftY()*-1);
+      m_bottomLeft.set(ps4.getLeftY()*-1);
+      m_topRight.set(ps4.getLeftY());
+      m_bottomRight.set(ps4.getLeftY());
+
+    }
+    else if (ps4.getLeftY() > 0.1){
+      m_topLeft.set(ps4.getLeftY()*-1);
+      m_bottomLeft.set(ps4.getLeftY()*-1);
+      m_topRight.set(ps4.getLeftY());
+      m_bottomRight.set(ps4.getLeftY());
+
+    }
+    else{
+      m_topLeft.stopMotor();
+      m_bottomLeft.stopMotor();
+      m_topRight.stopMotor();
+      m_bottomRight.stopMotor();
+    }
+    //move back
+
+    /*
+    //go to right
+    else if (ps4.getRightX() > 0.1){
+      m_topLeft.set(ps4.getRightX());
+      m_bottomLeft.set(ps4.getRightX()*-1);
+      m_topRight.set(ps4.getRightX()*-1);
+      m_bottomRight.set(ps4.getRightX());
+    }
+    //go to left
+    else if (ps4.getRightX() < -0.1){
+      m_topLeft.set(ps4.getLeftY());
+      m_bottomLeft.set(ps4.getLeftY()*-1);
+      m_topRight.set(ps4.getLeftY()*-1);
+      m_bottomRight.set(ps4.getLeftY());
+    }
+    */
+
+
 
     
     
