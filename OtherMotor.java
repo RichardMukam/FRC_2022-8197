63fe2897248ac334d89a6c57186cbd6b31a1a6e7
airtigers <89110180+airtigers@users.noreply.github.com>
2022-03-13 17:49:54 -0400

    Update OtherMotor.java

--- OtherMotor.java
+++ OtherMotor.java
@@ -3,97 +3,203 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-
-//ctre
-import com.ctre.phoenix.motorcontrol.Faults; //debugger
-import com.ctre.phoenix.motorcontrol.InvertType; //inverted
-import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX; //motorcontroller
-
-//REV sparkmotors
-import edu.wpi.first.wpilibj.MotorSafety;
-import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
-import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.Joystick; //general controller import
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonFX;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.PWMMotorController;
 import edu.wpi.first.wpilibj.PS4Controller;
-import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 //limelight
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
-//lime light in void func
 
-public class Robot extends TimedRobot {
-    // right side
-    WPI_VictorSPX rightBack = new WPI_VictorSPX(1);
-    WPI_VictorSPX rightFront = new WPI_VictorSPX(2);
-    MotorControllerGroup rightMotors = new MotorControllerGroup(rightBack, rightFront);
-    // left side
-    WPI_VictorSPX leftBack = new WPI_VictorSPX(3);
-    WPI_VictorSPX leftFront = new WPI_VictorSPX(4);
-    MotorControllerGroup leftMotors = new MotorControllerGroup(leftBack, leftFront);
-    // set up for differential drive
-    DifferentialDrive _diffDrive = new DifferentialDrive(leftMotors, rightMotors); //this was originally driveRight,driveLeft, but should've been driveLeft,driveRight
-    PWMTalonSRX _feeder = new PWMTalonSRX(7);
-    PWMSparkMax _shooter = new PWMSparkMax(5);
+public class Robot extends TimedRobot 
+{
+  //driving motors
+  CANSparkMax  m_topLeft = new CANSparkMax(1,MotorType.kBrushless); // the top left motor - intiailizes the motor
+  CANSparkMax  m_bottomLeft = new CANSparkMax(2, MotorType.kBrushless); // the bottom left motor - intiailizes the motor
+  CANSparkMax  m_topRight = new CANSparkMax(4, MotorType.kBrushless); // the top right motor - intiailizes the motor
+  CANSparkMax  m_bottomRight = new CANSparkMax(3, MotorType.kBrushless); // the bottom right motor - intiailizes the motor
+  CANSparkMax shooter = new CANSparkMax(5, MotorType.kBrushless);
+  TalonSRX feeder = new TalonSRX(7);
+
+  Joystick joystick = new Joystick(0);
+  Joystick mechanismJoyStick = new Joystick(1);
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  
+  private MecanumDrive MecanumDrive;
+
+  Timer time = new Timer();
+
+  @Override
+  public void robotInit() 
+  {
+    //initialize mecanum drive
+    MecanumDrive = new MecanumDrive(m_topLeft, m_bottomLeft, m_topRight, m_bottomRight);
+  }
+
+  // this method runs once the robot enters teleop
+  @Override
+  public void teleopInit()
+  {
     
+  }
+  
+  // this method is ran periodically if the robot is in teleop
+  @Override
+  public void teleopPeriodic() 
+  {  
+  //mecanum drive, all drivetrain behaviors
 
-    //timer (for autonomous)
-    Timer time = new Timer();
+  //storing y-axis values of joystick
+  double joystickY = joystick.getY(); 
+  //deadband for joystick, if y-axis is less than 0.3, register no value (catch for errors)
+  if (Math.abs(joystickY) < 0.3) 
+  {
+    joystickY = 0.0;
+  }
 
-    // controllers
-    PS4Controller ps4 = new PS4Controller(0);
-    Joystick joystick = new Joystick(1);
+  //forward
+  if (joystickY < -0.3) 
+  {
+    m_bottomRight.setInverted(false);
+    m_topRight.setInverted(false);
+    m_topLeft.setInverted(true);
+    m_bottomLeft.setInverted(true);
+  }
+  //backward
+  if (joystickY > 0.3) 
+  {
+    m_bottomRight.setInverted(false);
+    m_topRight.setInverted(false);
+    m_topLeft.setInverted(true);
+    m_bottomLeft.setInverted(true);
+  }
+
+  double joystickX = joystick.getX(); //storing x-axis values of joystick
+  if (Math.abs(joystickX) < 0.3) //deadband for joystick
+  {
+    joystickX = 0.0;
+  }
+  //sliding left
+  if(joystickX < -0.3) 
+  {
+    m_bottomRight.setInverted(true);
+    m_topRight.setInverted(true);
+    m_topLeft.setInverted(false);
+    m_bottomLeft.setInverted(false);
+  }
+  //sliding right
+  if (joystickX > 0.3)
+  {
+    m_bottomRight.setInverted(true);
+    m_topRight.setInverted(true);
+    m_topLeft.setInverted(false);
+    m_bottomLeft.setInverted(false);
+  }
+  //22.5 degrees to 67.5, joystick bounds top right
+  if (joystick.getDirectionDegrees() > 22.5 && joystick.getDirectionDegrees() < 67.5)
+  {
+    m_bottomLeft.stopMotor();
+    m_topRight.stopMotor();
+    m_bottomRight.setInverted(false);
+    m_topLeft.setInverted(true);
+    m_bottomRight.set(-0.5);
+    m_topLeft.set(-0.5);
+  }
+
+  double joystickZ = joystick.getZ(); //storing z-axis values of joystick
+  if (Math.abs(joystickZ) < 0.3) //deadband for joystick
+  {
+    joystickZ = 0.0;
+  }
+
+  if (joystick.getRawButton(1))
+  {
+    shooter.set(1.0);
+    feeder.set(ControlMode.PercentOutput, 1.0);
+  }
+  else
+  {
+    shooter.set(0.0);
+    feeder.set(ControlMode.PercentOutput, 0.0);
+  }
+ 
+  
+  //twist of joystick, rotate counterclockwise, (work in prog)
+  /*if (joystick.getTwist() > 0.3)
+  {
+
+    }*/
+
+    MecanumDrive.driveCartesian(joystickY, joystickX, joystickZ);
+
+    //limelight
     
-    @Override
-    public void autonomousPeriodic()
+    NetworkTableEntry tx = table.getEntry("tx");
+    NetworkTableEntry ty = table.getEntry("ty");
+    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry tv = table.getEntry("tv");
+
+    // double x = tx.getDouble(0);
+    // double y = ty.getDouble(0);
+    double area = ta.getDouble(0.0);
+    // double isTarget = tv.getDouble(0);
+
+    // SmartDashboard.putNumber("LimelightX", x);
+    // SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea: ", area);
+    // SmartDashboard.putNumber("LimelightIsTarget", isTarget);
+
+    System.out.println(area);
+    // System.out.println(isTarget);
+  }
+
+  // this method runs once the robot enters autonomous
+  @Override
+  public void autonomousInit()
+  {
+
+  } 
+
+  // this method is ran periodically if the robot is in autonomous
+  @Override
+  public void autonomousPeriodic()
+  {
+    if (time.get() <= 15.0) // our autonomous period will run for five (5) seconds 
     {
-        if (time.get() < 5.0)
-        {
-            
-        }
+      System.out.println("autonomous works: 3 sec");
+      System.out.println(time.get());
+      MecanumDrive.driveCartesian(0.5, 0.5, 0.0, 0.0);
+
+
     }
-    @Override
-    public void teleopPeriodic() {
-        _diffDrive.tankDrive(ps4.getLeftY(), ps4.getRightY());
-
-        if (ps4.getCrossButtonPressed())
-        {
-            
-        }
-        //create a method for the motors to spin
-       
+    else
+    {
+      time.stop();
+      MecanumDrive.stopMotor();
+      //stop motor
     }
+  }
 
-    @Override
-    public void robotInit() {
-        Ultrasonic.setAutomaticMode(true);
-        /* factory default values */
-        rightBack.configFactoryDefault();
-        rightFront.configFactoryDefault();
-        leftBack.configFactoryDefault();
-        leftFront.configFactoryDefault();
+  @Override
+  public void testPeriodic()
+  {
 
-      /* [3] flip values so robot moves forward when stick-forward/LEDs-green */
-        rightBack.setInverted(false); // !< Update this
-        leftBack.setInverted(false); // !< Update this
-
-        /* set up followers */
-        rightFront.follow(rightBack);
-        leftFront.follow(leftBack);
-
-        /*
-         * set the invert of the followers to match their respective master controllers
-         */
-        rightFront.setInverted(InvertType.FollowMaster);
-        leftFront.setInverted(InvertType.FollowMaster);
-    }
+  }
 }
